b5a18c4b06abaad8c6e2e75c9441e8f03899e2eb
mjunaid6 <mohmmadjunaid007@gmail.com>
2025-11-16 15:36:05 +0530

    Whole Project completed

---
+++ Model5/ClientHandler.java
@@ -0,0 +1,132 @@
+package basics.Model5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Set;
+
+public class ClientHandler implements Runnable {
+    private Socket socket;
+    private Server server;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+    private String username;
+
+    public ClientHandler(Socket socket, Server server) {
+        this.socket = socket;
+        this.server = server;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+
+        send("Welcome! Register: REG <username>");
+        send("Commands:");
+        send("Register: REG <username>");
+        send("Message: MSG <user> <msg>");
+        send("Get all users: AU");
+        send("Unregister: UR");
+        send("Exit: EXIT");
+
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                String[] parts = line.split(" ", 3);
+                String command = parts[0].toLowerCase();
+
+                switch (command) {
+                    case "reg":
+                        register(parts);
+                        break;
+
+                    case "msg":
+                        privateMsg(parts);
+                        break;
+                    case "au":
+                        listUsers();
+                        break;
+                    case "ur":
+                    case "exit":
+                        unregister();
+                        return;
+                    default:
+                        send("Unknown command!");
+                }
+            }
+        } catch (Exception e) {
+            if (username != null) server.unregisterUser(username);
+        }
+    }
+
+    public void send(String msg) {
+        try {
+            writer.write(msg + "\r\n");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void register(String[] parts) {
+        if (parts.length < 2) {
+            send("Usage: REG <username>");
+            return;
+        }
+
+        if (server.registerUser(parts[1], this)) {
+            username = parts[1];
+            send("Registered as " + username);
+        } else {
+            send("Username exists");
+        }
+    }
+
+    private void unregister() {
+        if (username != null) {
+            server.unregisterUser(username);
+            send("Unregistered " + username);
+        }
+        close();
+    }
+
+    private void privateMsg(String[] parts) {
+        if (parts.length < 3) {
+            send("Wrong Usage of MSG <user> <message>");
+            return;
+        }
+        if (username == null) {
+            send("Register first!");
+            return;
+        }
+
+        if (server.sendMessage(username, parts[1], parts[2])) {
+            send("Message sent to " + parts[1]);
+        } else {
+            send("User not found");
+        }
+    }
+
+    private void listUsers() {
+        Set<String> users = server.getAllUsers();
+        send("Users:");
+        users.forEach(user -> send(user));
+    }
+
+    public void close() {
+        try {
+            if(!socket.isClosed()) socket.close();
+            reader.close();
+            writer.close();
+        } catch (Exception ignored) {}
+    }
+}
---
+++ Model5/Server.java
@@ -0,0 +1,90 @@
+package basics.Model5;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Server {
+    private int PORT = 9000;
+    private ServerSocket serverSocket;
+    private ExecutorService executorPool;
+    private Map<String, ClientHandler> clients;
+    public static final int DELETE_TIMER = 30;  
+    private ScheduledExecutorService scheduler;
+    private static int MESSAGE_ID = 1;
+
+
+    public Server() {
+        try{
+            serverSocket = new ServerSocket(PORT);
+            serverSocket.setReuseAddress(true);
+            executorPool = Executors.newCachedThreadPool();
+            clients = new ConcurrentHashMap<>();
+            scheduler = Executors.newScheduledThreadPool(5);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("Server is listening on PORT : " + PORT);
+    } 
+
+    public void start() {
+        while(true) {
+            try{
+                Socket client = serverSocket.accept();
+                System.out.println("New User connnection established with : " + client.getRemoteSocketAddress());
+
+                ClientHandler clientHandler = new ClientHandler(client,this);
+                executorPool.submit(clientHandler);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+
+    public Set<String> getAllUsers() {
+        return clients.keySet();
+    }
+
+    public boolean registerUser(String username, ClientHandler clientHandler) {
+        return clients.putIfAbsent(username, clientHandler) == null;
+    }
+
+    public void unregisterUser(String username) {
+        clients.remove(username);
+    }
+
+    public ClientHandler getClientHandler(String username) {
+        return clients.get(username);
+    }
+
+    public boolean sendMessage(String from, String toUser, String message) {
+        if(!clients.containsKey(toUser)) return false;
+
+        ClientHandler target = clients.get(toUser);
+        if(target == null) return false;
+
+        int msgId = MESSAGE_ID++;
+
+        target.send("MSG#" + msgId + " Message from " + from + ": " + message);
+
+        scheduler.schedule(() -> {
+            target.send("DELETE#" + msgId);
+        }, DELETE_TIMER, TimeUnit.SECONDS);
+
+        return true;
+    }
+
+
+}
